6df1880e370cb51fcb69fd47fdcb48b00eea76ff
mikkelfo <mikk528a@gmail.com>
2019-12-01 14:15:59 +0900

    Fixed load_from_files()

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from preprocess import preprocess
 from clustering import cluster, ranking, indices
 from sklearn.feature_extraction.text import TfidfVectorizer
 import csv
+import numpy as np
 
 def write_to_files():
 
@@ -34,14 +35,24 @@ def write_to_files():
 
 def load_from_files():
     df = load('articles/')
+    df['year'] = df['time'].str[:4]
+    year2015, year2016, year2017 = df[df['year'] == '2015'], df[df['year'] == '2016'], df[df['year'] == '2017']
 
     with open("dbscan.csv") as f:
         fr = csv.reader(f)
         reader = list(fr)
         cluster_indices = [[int(x) for x in row] for row in reader]
     db = []
-    for row in cluster_indices:
-        db.append(df[df.index.isin(row)])
+    for row in cluster_indices[0:10]:
+        row = np.array(row) + 16613
+        db.append(year2015[year2015.index.isin(row)])
+    for row in cluster_indices[10:20]:
+        row = np.array(row) + 9128
+        db.append(year2016[year2016.index.isin(row)])
+    for row in cluster_indices[20:30]:
+        row = np.array(row) + 2
+        db.append(year2017[year2017.index.isin(row)])
+
     dbscan_2015 = db[0:10]
     dbscan_2016 = db[10:20]
     dbscan_2017 = db[20:30]
@@ -51,8 +62,15 @@ def load_from_files():
         reader = list(fr)
         cluster_indices = [[int(x) for x in row] for row in reader]
     km = []
-    for row in cluster_indices:
-        km.append(df[df.index.isin(row)])
+    for row in cluster_indices[0:10]:
+        row = np.array(row) + 16613
+        km.append(year2015[year2015.index.isin(row)])
+    for row in cluster_indices[10:20]:
+        row = np.array(row) + 9128
+        km.append(year2016[year2016.index.isin(row)])
+    for row in cluster_indices[20:30]:
+        row = np.array(row) + 2
+        km.append(year2017[year2017.index.isin(row)])
     kmeans_2015 = km[0:10]
     kmeans_2016 = km[10:20]
     kmeans_2017 = km[20:30]
